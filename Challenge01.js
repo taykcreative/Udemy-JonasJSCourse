6f0511d72ad5423c9be36ab06dcf01cb3413e542
Tay <tayk.dev@gmail.com>
2020-12-28 22:37:27 -0800

    Add files via upload
    I fixed Challenge 1. It was correct initially, but I was adding stuff that I was supposed to add in Challenge 2. Challenge 2 is completed. I love template literals! <3

--- Challenge01.js
+++ Challenge01.js
@@ -7,10 +7,3 @@ let markBMI =  massMark / (heightMark * heightMark);
 let johnBMI =  massJohn / (heightJohn * heightJohn);
 
 let markHigherBMI;
-
-if (markBMI > johnBMI) markHigherBMI = true;
-
-console.log ("Mark's BMI is " + markBMI + ".");
-console.log ("John's BMI is " + johnBMI + ".");
-console.log ("Is Mark's BMI higher than John's BMI?");
-console.log (markHigherBMI);
